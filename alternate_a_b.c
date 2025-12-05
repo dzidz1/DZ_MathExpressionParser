9df4eec273a7a801d40b9423ea3b61c270fe789a
Akaki Dzidziguri <akakidzidziguri03@gmail.com>
2025-12-05 11:16:52 +0400

    implement G2 with restoring checkpoint for experimental reasons

--- alternate_a_b.c
+++ alternate_a_b.c
@@ -1,11 +1,83 @@
 #include "mexpr_cpp_enums.h"
 #include "parser_export.h"
 
+// G1:
 // A->aB
 // B->bA|b|ba
 
+// G2:
+// C->bD
+// D-> aC|a|ab
+
 parse_rc_t A();
 parse_rc_t B();
+parse_rc_t C();
+parse_rc_t D();
+
+parse_rc_t D() {
+  parse_init();
+  int initial_chkp;
+  CHECKPOINT(initial_chkp); // save the lex-stack top
+
+  // D->aC
+  do {
+    token_code = cyylex();
+    if (token_code != MATH_CPP_VARIABLE || strcmp(lex_curr_token, "a") != 0) {
+      break;
+    }
+
+    err = C();
+    if (err == PARSE_ERR)
+      break;
+    RETURN_PARSE_SUCCESS;
+  } while (0);
+
+  RESTORE_CHECKPOINT(initial_chkp);
+
+  // D->a
+  do {
+    token_code = cyylex();
+    if (token_code != MATH_CPP_VARIABLE || strcmp(lex_curr_token, "a") != 0) {
+      break;
+    }
+    RETURN_PARSE_SUCCESS;
+  } while (0);
+
+  RESTORE_CHECKPOINT(initial_chkp);
+
+  // D->ab
+  do {
+    token_code = cyylex();
+    if (token_code != MATH_CPP_VARIABLE || strcmp(lex_curr_token, "a") != 0) {
+      break;
+    }
+
+    token_code = cyylex();
+    if (token_code != MATH_CPP_VARIABLE || strcmp(lex_curr_token, "b") != 0) {
+      break;
+    }
+
+    RETURN_PARSE_SUCCESS;
+  } while (0);
+
+  RETURN_PARSE_ERROR;
+}
+
+// C->bD
+parse_rc_t C() {
+  parse_init();
+  token_code = cyylex();
+  if (token_code != MATH_CPP_VARIABLE || strcmp(lex_curr_token, "b") != 0) {
+    RETURN_PARSE_ERROR;
+  }
+
+  err = D();
+
+  if (err == PARSE_ERR)
+    RETURN_PARSE_ERROR;
+
+  RETURN_PARSE_SUCCESS;
+}
 
 // A->aB
 parse_rc_t A() {
